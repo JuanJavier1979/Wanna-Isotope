8a14a144b7cbf38f333689c26f6ba72fcdd3abfe
JuanJavier1979 <jjmrestituto@gmail.com>
2016-03-06 13:50:53 +0100

    1.0.3 Term parameter Added

--- public/wanna-isotope-shortcode.php
+++ public/wanna-isotope-shortcode.php
@@ -55,6 +55,7 @@ class Wanna_Isotope_Shortcode {
             'order'     => '',
             'order_by'  => 'menu_order',
             'tax'       => '',
+            'term'      => '',
         ), $atts) );
 
         if( null != $id ) {
@@ -62,22 +63,38 @@ class Wanna_Isotope_Shortcode {
         }
 
         if( null == $id ) {
-	        $id = 'wanna-'.md5( date( 'jnYgis' ) );
+	        $id = 'wanna'.md5( date( 'jnYgis' ) );
 	        $id_output = 'id="' . $id . '"';
 	    }
 
-        $isotope_loop = new WP_Query ( array(
-            'post_type'       => $type,
-            'order'           => $order,
-            'orderby'         => $order_by,
-            'posts_per_page'  => $items
-        )  );
+        if( $term == null ) {
+            $isotope_loop = new WP_Query ( array(
+                'post_type'       => $type,
+                'order'           => $order,
+                'orderby'         => $order_by,
+                'posts_per_page'  => $items
+            )  );
+        } else {
+            $isotope_loop = new WP_Query ( array(
+                'post_type'       => $type,
+                'order'           => $order,
+                'orderby'         => $order_by,
+                'posts_per_page'  => $items,
+                'tax_query' => array(
+                    array(
+                        'taxonomy' => $tax,
+                        'field'    => 'slug',
+                        'terms'    => $term,
+                    ),
+                ),
+            )  );
+        }
 
         $isotope_output = '';
         
         if ( $isotope_loop->have_posts() ) :
 
-            if( $tax != null ) {
+            if( $tax != null && $term == null ) {
                 $isotope_output .= '<ul id="filters-' . $id . '" class="filters">';
                 $terms = get_terms( $tax );
                 $count = count($terms);
@@ -86,6 +103,21 @@ class Wanna_Isotope_Shortcode {
                     foreach ( $terms as $term ) {
                         $termname = strtolower($term->slug);
                         $isotope_output .= '<li><a href="javascript:void(0)" title="filter ' . $term->name . '" data-filter=".' . $termname . '">' . $term->name . '</a></li>';
+                    }
+                }
+                $isotope_output .= '</ul>';
+            } elseif ( $term != null ) {
+                $isotope_output .= '<ul id="filters-' . $id . '" class="filters">';
+                $term_id = get_term_by( 'slug', $term, $tax );
+                $terms = get_term_children( $term_id->term_id, $tax );
+                $count = count($terms);
+                $isotope_output .= '<li><a href="javascript:void(0)" title="filter all" data-filter=".all" class="active">All</a></li>';
+                if ( $count > 0 ){
+                    foreach ( $terms as $term ) {
+                        $single_term = get_term( $term, $tax );
+                        $termslug = strtolower($single_term->slug);
+                        $termname = strtolower($single_term->name);
+                        $isotope_output .= '<li><a href="javascript:void(0)" title="filter ' . $termslug . '" data-filter=".' . $termslug . '">' . $termname . '</a></li>';
                     }
                 }
                 $isotope_output .= '</ul>';
@@ -114,39 +146,39 @@ class Wanna_Isotope_Shortcode {
 
         wp_reset_query();
 
-    $isotope_output .= '
-    <script type="text/javascript">
-        jQuery(document).ready(function($) {
-            var $container = $(\'#' . $id . '\');
-            $container.imagesLoaded( function(){
-                $container.isotope({
-                  itemSelector: ".isotope-item",
-                  layoutMode: "masonry"
+        $isotope_output .= '
+        <script type="text/javascript">
+            jQuery(document).ready(function($) {
+                var $container = $(\'#' . $id . '\');
+                $container.imagesLoaded( function(){
+                    $container.isotope({
+                      itemSelector: ".isotope-item",
+                      layoutMode: "masonry"
+                    });
                 });
-            });
 
-            var $optionSets = $(\'#filters-' . $id . '\'),
-            $optionLinks = $optionSets.find(\'a\');
-         
-            $optionLinks.click(function(){
-                var $this = $(this);
-                // don\'t proceed if already active
-                if ( $this.hasClass(\'active\') ) {
-                  return false;
-                }
-                var $optionSet = $this.parents(\'#filters\');
-                $optionSets.find(\'.active\').removeClass(\'active\');
-                $this.addClass(\'active\');
+                var $optionSets = $(\'#filters-' . $id . '\'),
+                $optionLinks = $optionSets.find(\'a\');
              
-                //When an item is clicked, sort the items.
-                 var selector = $(this).attr(\'data-filter\');
-                $container.isotope({ filter: selector });
+                $optionLinks.click(function(){
+                    var $this = $(this);
+                    // don\'t proceed if already active
+                    if ( $this.hasClass(\'active\') ) {
+                      return false;
+                    }
+                    var $optionSet = $this.parents(\'#filters-' . $id . '\');
+                    $optionSets.find(\'.active\').removeClass(\'active\');
+                    $this.addClass(\'active\');
+                 
+                    //When an item is clicked, sort the items.
+                     var selector = $(this).attr(\'data-filter\');
+                    $container.isotope({ filter: selector });
 
-                return false;
-            });
-        }); 
-    </script>';
-        
+                    return false;
+                });
+            }); 
+        </script>';
+
         return $isotope_output;
 
 	}
